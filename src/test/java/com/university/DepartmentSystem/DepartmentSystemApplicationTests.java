83e651d1dad865553edc99ae55c16d96b8d14875
Mathish Sasmitha <it24101669@my.sliit.lk>
2025-09-19 00:17:20 +0530

    store changes

---
+++ src/test/java/com/university/DepartmentSystem/DepartmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.university.DepartmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
